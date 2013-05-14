9390f2881dfa884bdca10d0748982fef5abf727e
granizoa <a.granizo@hs-mannheim.de>
2013-05-14 17:06:42 +0200

    Created Test file and generated files. Removed obsolete files

---
+++ tests/Test.cpp
@@ -0,0 +1,96 @@
+#include Test.h
+
+class Test::PrivateData
+{
+		public:
+		int	stereoProcessorType;
+		double	factor;
+		std::wstring	parameterSetFile;
+		cv::Point	offset;
+		PrivateData():
+			stereoProcessorType(0)
+			, factor(1)
+			, parameterSetFile(L"stereoprocessor_parameters.yml")
+			, offset(){
+		}
+};
+
+//-----------------------------------------------------------------------------
+Test::Test()  : d_data(new PrivateData())
+{
+
+}
+
+Test::~Test()
+{
+
+}
+
+// PROPERTIES
+//-----------------------------------------------------------------------------
+void Test::setParameterSetFile(const std::wstring value)
+{
+	d_data->parameterSetFile = value;
+}
+
+std::wstring	Test::parameterSetFile() const
+{
+	return d_data->parameterSetFile;
+}
+
+//-----------------------------------------------------------------------------
+void Test::setOffset(const cv::Point value)
+{
+	d_data->offset = value;
+}
+
+cv::Point	Test::offset() const
+{
+	return d_data->offset;
+}
+
+// PUBLIC METHODS
+//-----------------------------------------------------------------------------
+bool	Test::init()
+{
+
+}
+
+//-----------------------------------------------------------------------------
+bool	Test::process(const cv::InputArray src1, const cv::InputArray src2, cv::OutputArray dst)
+{
+
+}
+
+// PRIVATE METHODS
+//-----------------------------------------------------------------------------
+bool	Test::loadParameters(const std::wstring& filePath)
+{
+
+}
+
+//-----------------------------------------------------------------------------
+bool	Test::saveParameters(const std::wstring& filePath)
+{
+
+}
+
+// PROTECTED METHODS
+//-----------------------------------------------------------------------------
+void	Test::performPanSharpening(cv::InputArray visibleMono, cv::InputArray thermal, cv::OutputArray dst, cv::Point offset)
+{
+
+}
+
+//-----------------------------------------------------------------------------
+void	Test::translateImage(cv::InputArray src, cv::OutputArray dst, cv::Point offset = cv::Point(0, 0))
+{
+
+}
+
+//-----------------------------------------------------------------------------
+void	Test::zoomImage(cv::InputArray src, cv::OutputArray dst, double factor = 1)
+{
+
+}
+
---
+++ tests/Test.h
@@ -0,0 +1,39 @@
+#ifndef TEST_H
+#define TEST_H
+
+
+
+class Test
+{
+
+	public:
+		// Property get / set
+		void setParameterSetFile(const std::wstring value);
+		std::wstring	parameterSetFile() const;
+
+		void setOffset(const cv::Point value);
+		cv::Point	offset() const;
+
+		// Methods
+		bool	init();
+		bool	process(const cv::InputArray src1, const cv::InputArray src2, cv::OutputArray dst);
+
+	private:
+		bool	loadParameters(const std::wstring& filePath);
+		bool	saveParameters(const std::wstring& filePath);
+
+	protected:
+		void	performPanSharpening(cv::InputArray visibleMono, cv::InputArray thermal, cv::OutputArray dst, cv::Point offset);
+		void	translateImage(cv::InputArray src, cv::OutputArray dst, cv::Point offset = cv::Point(0, 0));
+		void	zoomImage(cv::InputArray src, cv::OutputArray dst, double factor = 1);
+
+	private:
+		class	PrivateData;
+		std::unique_ptr<PrivateData>	d_data;
+
+	public:
+		Test();
+		virtual ~Test();
+};
+
+#endif TEST_H
