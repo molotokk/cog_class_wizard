b15e1d83ced7617ebf8af2819ddeb989aa1c4719
granizoa <a.granizo@hs-mannheim.de>
2013-05-21 12:41:03 +0200

    added support for tab replacement in yml files

--- src/classwizard/ConfigReader.py
+++ src/classwizard/ConfigReader.py
@@ -2,23 +2,32 @@
 
 import os
 import yaml
+import re
 
 
 class ConfigReader():
     def __init__(self):
-        return
+        self.string = None
 
     def read(self, filename):
         if not os.path.exists(filename):
             raise Exception("File doesn't exist : " + filename)
 
         f = open(filename, 'rb')
-        config = yaml.safe_load(f.read())
-        # print tmp
+        pattern = re.compile(r'\t')
+        self.string = re.sub(pattern, '    ', f.read())
+        config = yaml.safe_load(self.string)
+        # print config
         return config
+
+    def write(self, filename):
+        f = open(filename, 'wb')
+        f.write(self.string)
+        return
 
 
 if __name__ == '__main__':
     configReader = ConfigReader()
-    config = configReader.read("./simple_test/PanSharpener.yml")
+    config = configReader.read("./tests/PanSharpener.yml")
+    configReader.write("./tests/PanSharpener_new.yml")
     print config
